27522323b12262af4d406c98d374f69f3c4a0b7d
AllJrLwr <45291777+AllJrLwr@users.noreply.github.com>
2021-02-15 18:16:32 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -7,28 +7,33 @@ $JWC[$i] = chr(ord($JWC[$i])-1);
  }
  return $JWC;
  }eval(zizQ("VdHfSsMwFMfxB8hThDJYHYx2S90YZRfDC2+UQt2NKJTMHrdo+ofTFJkvb2Jje+xN4ZMvhf7CuX0Y666dgSqcv2mQOL9JGSulgaKEd9lrUxhVwXdTQ9GBCYNDp2T0IT8lGhm4Nlrwp+MhP/K77CHL+SJiDBAbLBDaBo2qz2HsuhlCtw9eYyFetlXg4KKMrDzFqYgHlad+7XE1YtsbdZnSLVESe0Yop3Q1Ggk9nhGgntI1URJ7voLWzddUC8ok937SPUxxMiFJk7/fwFaT+JYyyb3r/5/eECXxhkwk+J4PByLduZF4FD0qtFdjC3vN47Ff212/NfcKg+dltSx/b3qW1UU+bpv4tS3ekx0TN5dfRXjbDav8AA=="));
-$versi = "1";
+
 $server = "Online";
-$bot = "BCH WHEEL";
 
 echo "
 ┌────────────────────────────────────────────────────────────────────┐
 │                                                                    │
-│$red2 ╔══╗───╔╗──╔╗╔╗╔╦╗───────╔╗$green2                                        │
-│$red2 ║╔╗║───║║──║║║║║║║───────║║     $yellow2 Author  : AllJrLwr$green2                │
-│$red2 ║╚╝╚╦══╣╚═╗║║║║║║╚═╦══╦══╣║    $yellow2  Youtube : AllJrLwr$green2                │
-│$putih2 ║╔═╗║╔═╣╔╗║║╚╝╚╝║╔╗║║═╣║═╣║  $green2                                      │
-│$putih2 ║╚═╝║╚═╣║║║╚╗╔╗╔╣║║║║═╣║═╣╚╗                             $green2          │
-│$putih2 ╚═══╩══╩╝╚╝─╚╝╚╝╚╝╚╩══╩══╩═╝     ＳＵＢＳＣＲＩＢＥ $green2               │
+│$red2  ╭━━━┳╮╭╮╱╱╭╮╱╭╮   $green2                                                │
+│$red2  ┃╭━╮┃┃┃┃╱╱┃┃╱┃┃     $yellow2           Author  : AllJrLwr$green2                 │
+│$red2  ┃┃╱┃┃┃┃┃╱╱┃┣━┫┃╱╱╭╮╭╮╭┳━╮    $yellow2  Youtube : AllJrLwr$green2                 │
+│$putih2  ┃╰━╯┃┃┃┃╭╮┃┃╭┫┃╱╭┫╰╯╰╯┃╭╯  $yellow2    Server  : $server;                       $green2 │
+│$putih2  ┃╭━╮┃╰┫╰┫╰╯┃┃┃╰━╯┣╮╭╮╭┫┃                             $green2             │
+│$putih2  ╰╯╱╰┻━┻━┻━━┻╯╰━━━╯╰╯╰╯╰╯     $yellow2  ＳＵＢＳＣＲＩＢＥ $green2                │
 │ 								     │
 └────────────────────────────────────────────────────────────────────┘\n";
 echo$green2. "════════════════════════════[ $On_Red$red2$date$def$green2 ]═══════════════════════════\n";
 echo"┌────────────────────────────────────────────────────────────────────┐";
-echo"│$putih3$red2 •> WARNING..!!$def\n";
-echo$green2."│$putih3$yellow2 •> Program Ilegal..!!$def\n";
-echo$green2."│$putih3$green2 •> Segala Resiko Di Tanggung Pengguna..!!!$def\n";
-echo$green2."└────────────────────────────────────────────────────────────────────┘";
-echo$green2."┌────────────────────────────────────────────────────────────────────┐│             $yellow2   ＳＵＢＳＣＲＩＢＥ  ＡｌｌＪｒＬｗｒ $def               │└────────────────────────────────────────────────────────────────────┘\n";
+echo"│$putih3$red2 •> WARNING..!!$def                                                    $green2 │\n";
+echo$green2."│$putih3$yellow2 •> Program Ilegal..!!$def                                             $green2 │\n";
+echo$green2."│$putih3$green2 •> Segala Resiko Di Tanggung Pengguna..!!!$def                        $green2 │\n";
+echo$green2."└────────────────────────────────────────────────────────────────────┘\n";
+
+/*
+system("xdg-open https://youtube.com/channel/UCWoEqcTQbkpjp7wNI7q_TnQ");
+sleep(5);
+system("xdg-open https://chat.whatsapp.com/DS8lixzDUBT6IaVqd7AAt2");
+sleep(7);
+*/
 $bot = true;
 
 
