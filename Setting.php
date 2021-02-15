38b595065324e0346e0561fde4f8e842f2599ee5
AllJrLwr <45291777+AllJrLwr@users.noreply.github.com>
2021-02-15 16:47:28 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -1,4 +1,30 @@
 <?php
+system('clear');
+date_default_timezone_set("Asia/jakarta");
+/* START COLOR */
+error_reporting(0);
+$res="\033[7m";
+$hitam="\033[0;30m";
+$abu2="\033[1;30m";
+$putih="\033[0;37m";
+$putih2="\033[1;37m";
+$red="\033[0;31m";
+$red2="\033[1;31m";
+$green="\033[0;32m";
+$green2="\033[1;32m";
+$yellow="\033[0;33m";
+$yellow2="\033[1;33m";
+$blue="\033[0;34m";
+$blue2="\033[1;34m";
+$purple="\033[0;35m";
+$purple2="\033[1;35m";
+$lblue="\033[0;36m";
+$lblue2="\033[1;36m";
+$putih3 = "\033[3;97m"; //Miring
+$def = "\033[0m";
+$date = date("Y-m-d");
+$On_Red="\033[47m";
+$On_Green="\033[42m";$yellow3="\033[9;33m";
 
 $versi = "1";
 $server = "Online";
