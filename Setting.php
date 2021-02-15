a27dbea53bdbad321d5909ddf850c97bf61e9811
AllJrLwr <45291777+AllJrLwr@users.noreply.github.com>
2021-02-15 18:20:43 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -16,8 +16,8 @@ echo "
 │$red2  ╭━━━┳╮╭╮╱╱╭╮╱╭╮   $green2                                                │
 │$red2  ┃╭━╮┃┃┃┃╱╱┃┃╱┃┃     $yellow2           Author  : AllJrLwr$green2                 │
 │$red2  ┃┃╱┃┃┃┃┃╱╱┃┣━┫┃╱╱╭╮╭╮╭┳━╮    $yellow2  Youtube : AllJrLwr$green2                 │
-│$putih2  ┃╰━╯┃┃┃┃╭╮┃┃╭┫┃╱╭┫╰╯╰╯┃╭╯  $yellow2    Server  : $server                     $green2 │
-│$putih2  ┃╭━╮┃╰┫╰┫╰╯┃┃┃╰━╯┣╮╭╮╭┫┃                           $green2             │
+│$putih2  ┃╰━╯┃┃┃┃╭╮┃┃╭┫┃╱╭┫╰╯╰╯┃╭╯  $yellow2    Server  : $server                   $green2 │
+│$putih2  ┃╭━╮┃╰┫╰┫╰╯┃┃┃╰━╯┣╮╭╮╭┫┃                             $green2             │
 │$putih2  ╰╯╱╰┻━┻━┻━━┻╯╰━━━╯╰╯╰╯╰╯     $yellow2  ＳＵＢＳＣＲＩＢＥ $green2                │
 │ 								     │
 └────────────────────────────────────────────────────────────────────┘\n";
