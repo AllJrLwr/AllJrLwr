ab085f7b644dcd9e33b74800892dc648302254ad
AllJrLwr <45291777+AllJrLwr@users.noreply.github.com>
2021-02-15 16:45:07 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -1,27 +1,23 @@
 <?php
 
-$versi = "1.0";
-$server = "updating";
-$bot = "webminer";
+$versi = "1";
+$server = "Online";
+$bot = "BCH WHEEL";
 
-$capthca = "${kuning}
- ██████╗ █████╗ ██████╗ ████████╗
-██╔════╝██╔══██╗██╔══██╗╚══██╔══╝
-██║     ███████║██████╔╝   ██║
-██║     ██╔══██║██╔═══╝    ██║4
-╚██████╗██║  ██║██║        ██║Doge
- ╚═════╝╚═╝  ╚═╝╚═╝        ╚═╝
-${putih}[-] version${ijo} ${versi}${putih} (${kuning}${bot}${putih})
-${putih}[-] server${ijo} ${server}
-${putih}[created_by]${biru} Waryono${putih} [channel_by]${biru} Kang Repot
-${putih}[support_by]${biru} Sandi Pratama${putih} [channel_by]${biru} Sandi X-Dark
-${putih}[-] forum diskusi ${ijo}https://t.me/belajarnuyul
-${ijo}                  https://t.me/kangrepot
-${merah}- - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
-$pesan="${merah}[!] warning !!
-${kuning}[-] ini adalah progam ilegal
-[-] resiko sepenuhnya di tanggung pengguna${putih}
-${kuning}[message server]${putih} Sedang Ada Pembaruaan Script !!
-${putih}                 Mohon Tunggu :)
-";
-$nuyul=false;
+echo "
+┌────────────────────────────────────────────────────────────────────┐
+│                                                                    │
+│$red2 ╔══╗───╔╗──╔╗╔╗╔╦╗───────╔╗$green2                                        │
+│$red2 ║╔╗║───║║──║║║║║║║───────║║     $yellow2 Author  : AllJrLwr$green2                │
+│$red2 ║╚╝╚╦══╣╚═╗║║║║║║╚═╦══╦══╣║    $yellow2  Youtube : AllJrLwr$green2                │
+│$putih2 ║╔═╗║╔═╣╔╗║║╚╝╚╝║╔╗║║═╣║═╣║  $green2                                      │
+│$putih2 ║╚═╝║╚═╣║║║╚╗╔╗╔╣║║║║═╣║═╣╚╗                             $green2          │
+│$putih2 ╚═══╩══╩╝╚╝─╚╝╚╝╚╝╚╩══╩══╩═╝     ＳＵＢＳＣＲＩＢＥ $green2               │
+│ 								     │
+└────────────────────────────────────────────────────────────────────┘\n";
+echo$green2. "════════════════════════════[ $On_Red$red2$date$def$green2 ]═══════════════════════════\n";
+echo"┌────────────────────────────────────────────────────────────────────┐";
+echo"│$putih3$red2 •> WARNING..!!$def\n";
+echo$green2."│$putih3$yellow2 •> Program Ilegal..!!$def\n";
+echo$green2."│$putih3$green2 •> Segala Resiko Di Tanggung Pengguna..!!!$def\n";
+echo$green2."└────────────────────────────────────────────────────────────────────┘";
